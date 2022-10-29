4e47ce12420e0bb155656130ed66ac3d0bee20e1
Marymount Labs <95556852+marymountlabs@users.noreply.github.com>
2022-10-29 17:16:05 +0800

    Update 1_📈_Analysis.py

--- pages/1_📈_Analysis.py
+++ pages/1_📈_Analysis.py
@@ -7,6 +7,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import datetime as dt
+import matplotlib
 
 st.set_page_config(
     page_title="Analysis",
